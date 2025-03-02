b5a948ce52ae57582cbc339891e494e331c50083
manybotts <105394368+manybotts@users.noreply.github.com>
2025-03-02 06:15:33 +0300

    Add files via upload

---
+++ components/MovieList.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+import BACKEND_URL from "./config";
+
+const MovieList = ({ category }) => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(`${BACKEND_URL}/${category}`)
+      .then((res) => res.json())
+      .then((data) => setItems(data[category]));
+  }, [category]);
+
+  return (
+    <div>
+      {items.map((item) => (
+        <MovieCard key={item.file_id} movie={item} />
+      ))}
+    </div>
+  );
+};
+
+export default MovieList;
